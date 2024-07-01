9b90e068b3ad9d3bcf02f49630127b4b5dc258d1
LautaroTorres <lautioptimus123@gmail.com>
2024-07-01 20:49:11 -0300

    Creacion de la primer parte con metodo while y try-catch en clase main

--- src/Gestion/Main.java
+++ src/Gestion/Main.java
@@ -1,12 +1,43 @@
 package Gestion;
 
+import java.util.InputMismatchException;
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hola mundo");
-		System.out.println("Hola mundo");
-		System.out.println("Segunda u");
-			}
+		
+		//Definiendo variables
+		Scanner scanner = new Scanner (System.in);
+		boolean salir = false;
+		
+		//Bucle que interactua con el usuario
+		while (!salir) {
+            System.out.println("Bienvenido al juego de adivinar el número.");
+            System.out.println("1. Jugar");
+            System.out.println("2. Salir");
+            System.out.print("Elige una opción: ");
+            try {
+                int opcion = scanner.nextInt();
 
-}
+                switch (opcion) {
+                    case 1:
+                        jugar(scanner);
+                        break;
+                    case 2:
+                        salir = true;
+                        System.out.println("Gracias por jugar. ¡Adiós!");
+                        break;
+                    default:
+                        System.out.println("Opción no válida. Inténtalo de nuevo.");
+                }
+            } catch (InputMismatchException e) { //Error al ingresar un valor no numerico
+                System.out.println("Entrada no válida. Por favor, ingresa un número.");
+                scanner.next(); // Limpiar el buffer del scanner
+            }
+        }
+        scanner.close();
+    }
+		
